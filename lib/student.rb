b7ead513671432c26e1431edf01eaa1361c3074f
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-25 00:02:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql)
   end
   
   def self.all_students_in_grade_X(x)
